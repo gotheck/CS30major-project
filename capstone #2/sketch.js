dbb3449d01b397343112b01ee460dfb0719cbf66
My Mistake <72628222+kierans-fluffypandas@users.noreply.github.com>
2021-10-22 11:01:02 -0600

    trying to make multiple targets

--- capstone #2/sketch.js
+++ capstone #2/sketch.js
@@ -1,39 +1,50 @@
 //cap stone project number 2 //
-
+let myTarget;
 let theTargets = [];
 let x, y, radius, xTime, yTime;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // x = random(width);
-  // y = random(height);
-  // radius = random(25, 40);
-  // xTime = 0.9;
-  // yTime = 0.0;
+  let myTarget = new Target(width/2, height/2);
+  theTargets.push(myTarget);
 }
-
 
 function draw() {
   background(0);
-  x = noise(xTime)* width;
-  y = noise(yTime)* height;
-  circle(x, y, radius*2);
-  xTime += 0.01;
-  yTime += 0.01;
+  for (let i=0; i<theTargets.length; i++) {
+    for (let j=0; j<theTargets.length; j++) {
+    theTargets[i].display();
+  }
+}
+
+function mousePressed() {
+  let myTarget = new Target(mouseX, mouseY);
+  theTargets.push(myTarget);
 }
 
 class Target {
   constructor(x, y) {
-    x = random(width);
-    y = random(height);
-    radius = random(25, 40);
-    xTime = 0.9;
-    yTime = 0.0;
+    this.x = random(width);
+    this.y = random(height);
+    this.radius = random(25, 40);
+    this.xTime = 0.9;
+    this.yTime = 0.0;
+    
   }
+
+  display() {
+    noStroke();
+    this.x = noise(xTime)* width;
+    this.y = noise(yTime)* height;
+    circle(x, y, radius*2);
+    xTime += 0.01;
+    yTime += 0.01;
+  }
+
+
 }
 
 // function targetSpawn() {
 //   theTarget.y -= theTarget.speed;
 //   theTarget.x +=
-// }
