87f7e1831843cc1962f6cc5abf49fc46b6da0c31
My Mistake <72628222+kierans-fluffypandas@users.noreply.github.com>
2021-10-25 11:00:23 -0600

    10/25/21 tried to add a target
    trying to make one target show up

--- capstone #2/sketch.js
+++ capstone #2/sketch.js
@@ -1,12 +1,11 @@
 //cap stone project number 2 //
 let myTarget;
 let theTargets = [];
-let x, y, radius, xTime, yTime;
+let x, y , radius, xTime, yTime;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  let myTarget = new Target(width/2, height/2);
   theTargets.push(myTarget);
 }
 
@@ -14,37 +13,33 @@ function draw() {
   background(0);
   for (let i=0; i<theTargets.length; i++) {
     for (let j=0; j<theTargets.length; j++) {
-    theTargets[i].display();
-  }
+      {
+        theTargets[i].display();
 }
 
-function mousePressed() {
-  let myTarget = new Target(mouseX, mouseY);
-  theTargets.push(myTarget);
-}
+ function mousePressed() {
+//   let myTarget = new Target(mouseX, mouseY); 
+//   theTargets.push(myTarget);
+// }
 
-class Target {
-  constructor(x, y) {
-    this.x = random(width);
-    this.y = random(height);
-    this.radius = random(25, 40);
-    this.xTime = 0.9;
-    this.yTime = 0.0;
-    
+    class Target {
+      constructor(x, y) {
+        this.x = random(width);
+        this.y = random(height);
+        this.radius = random(25, 40);
+        this.xTime = 0.9;
+        this.yTime = 0.0;
+        
+      }
+
+      display() {
+        noStroke();
+        this.x = noise(xTime)* width;
+        this.y = noise(yTime)* height;
+        circle(mouseX, mouseY, radius*2);
+        xTime += 0.01;
+        yTime += 0.01; 
+    }
   }
-
-  display() {
-    noStroke();
-    this.x = noise(xTime)* width;
-    this.y = noise(yTime)* height;
-    circle(x, y, radius*2);
-    xTime += 0.01;
-    yTime += 0.01;
-  }
-
-
 }
-
-// function targetSpawn() {
-//   theTarget.y -= theTarget.speed;
-//   theTarget.x +=
+}
