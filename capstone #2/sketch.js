907a0be29dba9f6b4b88aa00d911354fccdaf2d2
My Mistake <72628222+kierans-fluffypandas@users.noreply.github.com>
2021-10-26 11:00:44 -0600

    capstone 10/26/21

--- capstone #2/sketch.js
+++ capstone #2/sketch.js
@@ -1,45 +1,53 @@
 //cap stone project number 2 //
 let myTarget;
 let theTargets = [];
-let x, y , radius, xTime, yTime;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  let myTarget = new Target
   theTargets.push(myTarget);
 }
 
 function draw() {
   background(0);
-  for (let i=0; i<theTargets.length; i++) {
-    for (let j=0; j<theTargets.length; j++) {
-      {
-        theTargets[i].display();
-}
-
- function mousePressed() {
-//   let myTarget = new Target(mouseX, mouseY); 
-//   theTargets.push(myTarget);
-// }
-
-    class Target {
-      constructor(x, y) {
-        this.x = random(width);
-        this.y = random(height);
-        this.radius = random(25, 40);
-        this.xTime = 0.9;
-        this.yTime = 0.0;
-        
-      }
-
-      display() {
-        noStroke();
-        this.x = noise(xTime)* width;
-        this.y = noise(yTime)* height;
-        circle(mouseX, mouseY, radius*2);
-        xTime += 0.01;
-        yTime += 0.01; 
-    }
+  for (let i=0; i< theTargets.length; i++) {
+    for (let j=0; j< theTargets.length; j++) {
+    theTargets[i].move();
+    theTargets[i].display();
   }
 }
+
+function mousePressed() {
+  let myTarget = new Target(mouseX, mouseY); 
+  theTargets.push(myTarget);
+}
+
+class Target {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = random(25, 40);
+    this.dx = random(-10,10);
+    this.theColor
+    this.dy = random(-10,10);
+        
+  }
+  display() {
+    noStroke();
+    circle(this.x, this.y, this.radius*2);
+  }
+  move() {
+    this.x += this.dx;
+    this.y += this.dy;
+
+    //edge check
+    if (this.x - this.radius < 0 || this.x + this.radius > width) {
+      this.dx *= -1;
+    }
+    if (this.y - this.radius < 0 || this.y + this.radius > height) {
+      this.dy *= -1;
+    }
+  }
+
 }
